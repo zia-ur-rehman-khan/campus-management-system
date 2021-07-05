fc151ac39c7f1dd8c3d26829a2b28bb6871781c1
zia ur rehman khan <03112397853zia@gmail.com>
2021-07-06 03:43:43 +0500

    first commit

--- serverSide/app.js
+++ serverSide/app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const bodyparser = require("body-parser")
 const cors = require("cors")
-const port = 6000
+const port = process.env.PORT || 6000
 const dbHelper = require("./dbHelper/dbHelper.js")
 
 const app = express()
